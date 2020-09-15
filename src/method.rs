798e4953279fa92236b655d42d1f23bf7be9353e
lizhiyong2000 <lizhiyong2000@gmail.com>
2020-09-15 17:18:44 +0800

    测试RTSP message

--- src/method.rs
+++ src/method.rs
@@ -8,34 +8,20 @@ use self::Method::*;
 /// Representation of HTTP methods.
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub enum Method {
-    Get,
-    Put,
-    Post,
-    Delete,
+    Describe,
+    Announce,
+    GetParameter,
     Options,
-    Head,
-    Trace,
-    Connect,
-    Patch
+    Pause,
+    Play,
+    Redirect,
+    Setup,
+    SetParameter,
+    Teardown,
 }
 
 impl Method {
-    /// WARNING: This is unstable! Do not use this method outside of Rocket!
-    #[doc(hidden)]
-    pub fn from_hyp(method: &http::method::Method) -> Option<Method> {
-        match *method {
-            http::method::Method::GET => Some(Get),
-            http::method::Method::PUT => Some(Put),
-            http::method::Method::POST => Some(Post),
-            http::method::Method::DELETE => Some(Delete),
-            http::method::Method::OPTIONS => Some(Options),
-            http::method::Method::HEAD => Some(Head),
-            http::method::Method::TRACE => Some(Trace),
-            http::method::Method::CONNECT => Some(Connect),
-            http::method::Method::PATCH => Some(Patch),
-            _ => None,
-        }
-    }
+
 
     /// Returns `true` if an HTTP request with the method represented by `self`
     /// always supports a payload.
@@ -57,13 +43,13 @@ impl Method {
     /// assert_eq!(Method::Get.supports_payload(), false);
     /// assert_eq!(Method::Post.supports_payload(), true);
     /// ```
-    #[inline]
-    pub fn supports_payload(self) -> bool {
-        match self {
-            Put | Post | Delete | Patch => true,
-            Get | Head | Connect | Trace | Options => false,
-        }
-    }
+    // #[inline]
+    // pub fn supports_payload(self) -> bool {
+    //     match self {
+    //         Put | Post | Delete | Patch => true,
+    //         Get | Head | Connect | Trace | Options => false,
+    //     }
+    // }
 
     /// Returns the string representation of `self`.
     ///
@@ -78,15 +64,16 @@ impl Method {
     #[inline]
     pub fn as_str(self) -> &'static str {
         match self {
-            Get => "GET",
-            Put => "PUT",
-            Post => "POST",
-            Delete => "DELETE",
+            Describe => "DESCRIBE",
+            Announce => "ANNOUNCE",
+            GetParameter => "GET_PARAMETER",
             Options => "OPTIONS",
-            Head => "HEAD",
-            Trace => "TRACE",
-            Connect => "CONNECT",
-            Patch => "PATCH",
+            Pause => "PAUSE",
+            Play => "PLAY",
+            Redirect => "REDIRECT",
+            Setup => "SETUP",
+            SetParameter => "SET_PARAMETER",
+            Teardown => "TEARDOWN",
         }
     }
 }
@@ -98,15 +85,16 @@ impl FromStr for Method {
     // clients don't follow this, so we just do a case-insensitive match here.
     fn from_str(s: &str) -> Result<Method, ()> {
         match s {
-            x if uncased::eq(x, Get.as_str()) => Ok(Get),
-            x if uncased::eq(x, Put.as_str()) => Ok(Put),
-            x if uncased::eq(x, Post.as_str()) => Ok(Post),
-            x if uncased::eq(x, Delete.as_str()) => Ok(Delete),
+            x if uncased::eq(x, Describe.as_str()) => Ok(Describe),
+            x if uncased::eq(x, Announce.as_str()) => Ok(Announce),
+            x if uncased::eq(x, GetParameter.as_str()) => Ok(GetParameter),
             x if uncased::eq(x, Options.as_str()) => Ok(Options),
-            x if uncased::eq(x, Head.as_str()) => Ok(Head),
-            x if uncased::eq(x, Trace.as_str()) => Ok(Trace),
-            x if uncased::eq(x, Connect.as_str()) => Ok(Connect),
-            x if uncased::eq(x, Patch.as_str()) => Ok(Patch),
+            x if uncased::eq(x, Pause.as_str()) => Ok(Pause),
+            x if uncased::eq(x, Play.as_str()) => Ok(Play),
+            x if uncased::eq(x, Redirect.as_str()) => Ok(Redirect),
+            x if uncased::eq(x, Setup.as_str()) => Ok(Setup),
+            x if uncased::eq(x, SetParameter.as_str()) => Ok(SetParameter),
+            x if uncased::eq(x, Teardown.as_str()) => Ok(Teardown),
             _ => Err(()),
         }
     }
